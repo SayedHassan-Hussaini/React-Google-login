d2c6e4b16a8e48fad4a43e1219e8d486ac5a50ee
sayedhassan-husssaini <sh.hussaini1378@gmail.com>
2022-04-07 12:29:19 +0430

    set design

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
-import './App.css';
+import "./App.css";
+import { GoogleLogin } from "react-google-login";
 
 function App() {
+  function handleSuccess(response){
+    console.log(response)
+  }
+  function handleFaild(error){
+    console.log(error)
+  }
   return (
     <div className="App">
       <header className="App-header">
         <h1>React Google login App</h1>
+        <div>
+          <GoogleLogin
+            clientId={process.env.REACT_APP_GOOGLE_ID}
+            buttonText="Login with google"
+            onSuccess={handleSuccess}
+            onFailure={handleFaild}
+            cookiePolicy='single_host_origin'
+          ></GoogleLogin>
+        </div>
       </header>
     </div>
   );
